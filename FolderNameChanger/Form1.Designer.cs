9a6661aa75507e550237c698b8005e4ab04d9b79
sinan <sinan@DESKTOP-U775P9T(laptop)>
2019-08-09 22:00:50 +0300

    initial

---
+++ FolderNameChanger/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FolderNameChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tüm Klasörleri Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Default Dizin: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 212);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Seçilen Firma İçin Uzantıyı Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hangi firmada işin çalışacaksın?";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(220, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 38);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "İşim bitti, başlangıç haline getir.";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 408);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FolderNameChanger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FolderNameChanger/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderNameChanger
+{
+    public partial class Form1 : Form
+    {
+        //public static string basePath = @"C:\Users\sinan\source\TEST\";
+        public static string basePath = @"C:\Users\yusi7001\Documents\";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            textBox1.Text = basePath;
+
+            if (IsBasePathValid())
+            {
+                if (listBox1.Items.Count <= 0)
+                {
+                    FillListBoxWithItems();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            button2.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (IsBasePathValid())
+            //{
+            //    if(listBox1.Items.Count<=0)
+            //    {
+            //        FillListBoxWithItems();
+            //    }
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsBasePathValid())
+            {
+                if (listBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("bir firma seç");
+                }
+                else
+                {
+                    if (CreateTextFile())
+                    {
+                        MessageBox.Show("Mevcut dizinde yeni txt file yaratıldı. Butona tekrar basın.");
+                    }
+                    else
+                    {
+                        if (GetEditedFolderNames().Contains("My Nielsen Answers"))
+                        {
+                            MessageBox.Show("Başka bir firma için çalışacaksanız önce dosya isimlerini başlangıç haline getirin.");
+                        }
+                        else
+                        {
+                            string folderNameToBeUpdated = basePath + listBox1.SelectedItem;
+
+                            Directory.Move(folderNameToBeUpdated, basePath + "My Nielsen Answers");
+
+                            string originalText = listBox1.SelectedItem.ToString();
+                            string editedText = originalText.Substring(originalText.IndexOf("_") + 1);
+                            OverwriteTextFile(editedText);
+
+                            listBox1.Items.Clear();
+                            FillListBoxWithItems();
+
+                            label3.Text = editedText + " firmasında çalışılıyor.";
+                        }
+                    }
+                }
+            }      
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (IsBasePathValid())
+            {
+                if (GetEditedFolderNames().Contains("My Nielsen Answers"))
+                {
+                    string line1 = File.ReadLines(basePath + "ActiveCompanyLogFile.txt").First();
+                    string fileNameToBeReplaced = basePath + "My Nielsen Answers_" + line1;
+
+                    Directory.Move(basePath + "My Nielsen Answers", fileNameToBeReplaced);
+
+                    string path = basePath + "ActiveCompanyLogFile.txt";
+
+                    File.WriteAllText(path, "");
+
+                    listBox1.Items.Clear();
+
+                    FillListBoxWithItems();
+
+                    label3.Text = "Seçili Firma için uzantı geri eklendi.";
+                }
+                else
+                {
+                    MessageBox.Show("uzantısız dosya bulunamadı.");
+                }
+            }
+        }
+
+        public List<string> GetEditedFolderNames()
+        {
+            string[] folderNamesInPath = Directory.GetDirectories(basePath);
+
+            int basePathLength = basePath.Length;
+
+            List<string> justFolderNamesList = new List<string>();
+
+            foreach (var item in folderNamesInPath)
+            {
+                justFolderNamesList.Add(item.Remove(0, basePathLength));
+            }
+
+            return justFolderNamesList;
+        }
+
+        public void FillListBoxWithItems()
+        {
+            listBox1.Items.AddRange(GetEditedFolderNames().ToArray());
+        }
+
+        public bool CreateTextFile()
+        {
+            string path = basePath + "ActiveCompanyLogFile.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    tw.WriteLine("");
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void OverwriteTextFile(string text)
+        {
+            string path = basePath + "ActiveCompanyLogFile.txt";
+
+            if (File.Exists(path))
+            {
+                File.WriteAllText(path, "");
+
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    tw.WriteLine(text);
+                }
+            }
+        }
+
+        public bool IsBasePathValid()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Path alanını boş.");
+                return false;
+            }
+            else if (!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("Böyle bir klasör bulunmuyor.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
